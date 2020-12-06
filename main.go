cfe12deca0a970715777d092ca40741667aad742
Pablo Verdugo <pablovgdev@gmail.com>
2020-12-06 15:05:19 +0100

    📦 NEW: basic web server

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
 func main() {
-	println("Hello World!")
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World!")
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			rw.WriteHeader(http.StatusBadRequest)
+			rw.Write([]byte("Bad Request"))
+			log.Fatal(err.Error())
+			return
+		}
+		log.Print(string(data))
+		rw.Write(data)
+	})
+	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Goodbye World!")
+	})
+	http.ListenAndServe(":8000", nil)
 }
