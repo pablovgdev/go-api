25e49ea2301736f79041efb0381482efa4826ecd
Pablo Verdugo <pablovgdev@gmail.com>
2020-12-06 16:16:56 +0100

    📦 NEW: add handlers folder

---
+++ handlers/index-handler.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// IndexHandler struct
+type IndexHandler struct {
+	log *log.Logger
+}
+
+// NewIndexHandler constructor
+func NewIndexHandler(log *log.Logger) *IndexHandler {
+	return &IndexHandler{log}
+}
+
+// ServeHTTP all requests
+func (indexHandler *IndexHandler) ServeHTTP(
+	responseWriter http.ResponseWriter,
+	request *http.Request,
+) {
+	data, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		responseWriter.WriteHeader(http.StatusBadRequest)
+		responseWriter.Write([]byte("Bad Request"))
+		indexHandler.log.Fatal(err.Error())
+	} else {
+		indexHandler.log.Println("Hello World!")
+		indexHandler.log.Print(string(data))
+		responseWriter.Write(data)
+	}
+}
--- main.go
+++ main.go
@@ -1,26 +1,18 @@
 package main
 
 import (
-	"io/ioutil"
+	"go-api/handlers"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World!")
-		data, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			rw.WriteHeader(http.StatusBadRequest)
-			rw.Write([]byte("Bad Request"))
-			log.Fatal(err.Error())
-			return
-		}
-		log.Print(string(data))
-		rw.Write(data)
-	})
-	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Goodbye World!")
-	})
-	http.ListenAndServe(":8000", nil)
+	log := log.New(os.Stdout, "go-api", log.LstdFlags)
+	indexHandler := handlers.NewIndexHandler(log)
+
+	mux := http.NewServeMux()
+	mux.Handle("/", indexHandler)
+
+	http.ListenAndServe(":8000", mux)
 }
